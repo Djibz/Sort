c50ce852e255a3d51a4e05942571b85c76da2d91
Erwan Brunelliere <erwan.brunelliere@etudiant.univ-lr.fr>
2021-10-31 09:53:48 +0100

    Clean One

--- src/main.cpp
+++ src/main.cpp
@@ -16,37 +16,44 @@ int main(int argc, char** argv)
 {
     srand(time(0));
     ::ShowWindow(::GetConsoleWindow(), SW_HIDE);
-    SortGUI a(1000, 800);
+    SortGUI a(1000, 500);
 
-    a.initVect(304);
-    a.setTitle("Heap Sort");
+    a.initVect(500);
+    a.setTitle("Heap Sort - 500");
     a.setAlgo(new Heap());
     a.process();
     a.renderAll();
 
     a.initVect(500);
-    a.setTitle("Gravity");
+    a.setTitle("Gravity Sort - 500");
     a.setAlgo(new Gravity());
     a.process();
     a.renderAll();
 
     a.initVect(10000);
+    a.setTitle("Quick Sort - 10.000");
     a.setAlgo(new QuickSort());
     a.process();
     a.renderAll();
     
     a.initVect(10000);
-    a.renderAll();
+    a.setTitle("Merge Sort - 10.000");
     a.setAlgo(new TriFusion());
     a.process();
     a.renderAll();
 
-    SortGUI aGUI(1500, 500);
+    a.initVect(500);
+    a.setTitle("Radix Sort (LSD)(10) - 500");
+    a.setAlgo(new Radix());
+    a.process();
+    a.renderAll();
+
+    /*SortGUI aGUI(1500, 500);
     aGUI.initVect(500);
     aGUI.renderAll();
     aGUI.setAlgo(new Radix());
     aGUI.process();
-    a.renderAll();
+    a.renderAll();*/
 
     return 0;
 }
